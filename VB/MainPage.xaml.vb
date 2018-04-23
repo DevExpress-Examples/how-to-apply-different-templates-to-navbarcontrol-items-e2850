9cd15270e813c1874c0eb7c6b62836dbabb6300e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/MainPage.xaml.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.NavBar
+
+Namespace SilverlightApplication1
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			navbar.Groups(0).ItemTemplateSelector = New CustomItemTemplateSelector() With {.NavBar = navbar}
+		End Sub
+	End Class
+
+	Public Class CustomItemTemplateSelector
+		Inherits DataTemplateSelector
+		Private privateNavBar As NavBarControl
+		Public Property NavBar() As NavBarControl
+			Get
+				Return privateNavBar
+			End Get
+			Set(ByVal value As NavBarControl)
+				privateNavBar = value
+			End Set
+		End Property
+		Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+			Dim navBarItem As NavBarItem = CType(item, NavBarItem)
+			If navBarItem Is Nothing Then
+				Return MyBase.SelectTemplate(item, container)
+			End If
+			Select Case navBarItem.Content.ToString()
+				Case "Item Red"
+					Return CType(NavBar.Resources("RedContentTemplate"), DataTemplate)
+				Case "Item Green"
+					Return CType(NavBar.Resources("GreenContentTemplate"), DataTemplate)
+				Case "Item Blue"
+					Return CType(NavBar.Resources("BlueContentTemplate"), DataTemplate)
+				Case Else
+					Return Nothing
+			End Select
+		End Function
+	End Class
+End Namespace
